1708f5dacfa931e77400f8cfe9f828913ba76264
fish <ljx.love.you@qq.com>
2026-03-13 19:42:03 +0800

    优化readme和启动类

--- src/main/java/hk/fish/fishpicturebackend/FishPictureBackendApplication.java
+++ src/main/java/hk/fish/fishpicturebackend/FishPictureBackendApplication.java
@@ -1,9 +1,6 @@
 package hk.fish.fishpicturebackend;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
